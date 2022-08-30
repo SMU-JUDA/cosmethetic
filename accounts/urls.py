d99dea0df915ad81a8a8750912372f01407085c2
myunbongs <myunbongs@naver.com>
2022-08-30 09:15:35 +0900

    feat: ✨ 회원 기능 추가(#2)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,14 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from .views import *
+from .models import Profile
+
+app_name = 'accounts'
 
 urlpatterns = [
+    path('signup/', signup, name='signup'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
+    path('profile/<int:pk>', ProfileView.as_view(), name="profile"),
+    path('profile/update', ProfileUpdateView.as_view(), name="profile-update")
 ]
